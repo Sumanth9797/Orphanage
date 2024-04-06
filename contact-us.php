c2e396a57cd1a68294927122fd5156e257458453
chinnu <sumanthsinguluri567@gmail.com>
2024-04-07 00:06:05 +0530

    Initial commit

---
+++ contact-us.php
@@ -0,0 +1,29 @@
+<?php include './components/header.php'; ?>
+
+    <div class="ui container">
+
+        <!-- Top Navigation Bar -->
+        <?php include './components/top-menu.php'; ?>
+
+        <!-- BODY Content -->
+        <div class="ui grid">
+            <!-- Left menu -->
+            <?php include './components/side-menu.php'; ?>
+            
+            <!-- right content -->
+            <div class="twelve wide column">
+                <h1>Contact Us</h1>
+
+                
+                <p><strong>Address:</strong><br>  ORPHAN - Yuvodhaya 85, <br> 'D' Street, 7th Cross, Gandhinagar, <br> Bangalore 560009 Karnataka, India </p>
+
+                <p><strong>Email:</strong> orphanges@gmail.com</p>
+                <p><strong>Tel:</strong> +9199999999999, +919999999999</p>
+
+                <span class="p-20"></span>
+            </div>
+        </div>
+
+    </div>
+    
+<?php include './components/footer.php'; ?>
